317894b1dde6efc26206cb079c928da27cf81909
leomuko <kezzieleo@gmail.com>
2019-12-28 18:43:21 +0300

    Modify using reducers

--- src/screens/squareScreen.js
+++ src/screens/squareScreen.js
@@ -1,11 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { Text, View } from "react-native";
 import ColorCounter from '../components/ColorCounter';
 
-const COLOR_INCREAMENT = 20;
+
+//Using Reducers
+const COLOR_INCREAMENT = 15;
+
+const reducer=(state, action) =>{
+    switch(action.colorToChange){
+        case 'red':
+            return {...state, red: state.red + action.amount};
+        case 'green':
+            return {...state, green: state.green + action.amount};
+        case 'blue':
+            return {...state, blue: state.blue + action.amount};
+        default:
+            return state;
+    }
+
+}
+
+const SquareScreen = () =>{
+    const [state, dispatch] = useReducer(reducer, {red:0, green:0, blue:0});
+    return(
+        <View>
+            <ColorCounter
+            color="red"
+            onIncrease={()=> dispatch({colorToChange:'red', amount:COLOR_INCREAMENT})}
+            onDecrease={()=> dispatch({colorToChange:'red', amount: -1 *COLOR_INCREAMENT})}
+            />
+            <ColorCounter
+            color="green"
+            onIncrease={()=> dispatch({colorToChange:'green', amount:COLOR_INCREAMENT})}
+            onDecrease={()=> dispatch({colorToChange:'green', amount: -1 *COLOR_INCREAMENT})}
+            />
+            <ColorCounter
+            color="green"
+            onIncrease={()=> dispatch({colorToChange:'green', amount:COLOR_INCREAMENT})}
+            onDecrease={()=> dispatch({colorToChange:'green', amount: -1 *COLOR_INCREAMENT})}
+            />
+            <View
+                style={{
+                    height:200,
+                    width: 200,
+                    backgroundColor:`rgb(${state.red},${state.green},${state.blue})`
+                }}
+            >
+
+            </View>
+        </View>  
+
+    );
+}
+
+export default SquareScreen;
 
 
 
+// First example of  creating the squareScreen without reducers
+// ***************************************************************
+//********************************************************** 
+
+/* const COLOR_INCREAMENT = 20;
 const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [blue, setBlue] = useState(0);
@@ -58,6 +114,5 @@ const SquareScreen = () => {
         />
         <View style={{height:150, width:200, backgroundColor:`rgb(${red},${green},${blue})`}}></View>
     </View>
-}
+} */
 
-export default SquareScreen;
